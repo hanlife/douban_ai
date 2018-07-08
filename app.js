3bb041007ba5eed060a1c7e81427ec5f7122b97f
hanlife <m13760315374@163.om>
2018-07-08 10:21:10 +0800

    add my_Homepage

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ App({
   },
   globalData: {
     userInfo: null,
-    rootUrl:"http://www.f2ehan.com",
+    // rootUrl:"http://www.f2ehan.com",
     // rootUrl:'http://192.168.5.52:3000',
-    // rootUrl: 'http://localhost:3000'
+    rootUrl: 'http://localhost:3000'
   }
 })
 
@@ -18,23 +18,3 @@ App({
 //   "selectedIconPath": "./image/ai-s.png"
 // },
 
-// "tabBar": {
-//   "color": "#666",
-//     "selectedColor": "#E87019",
-//       "backgroundColor": "white",
-//         "borderStyle": "black",
-//           "list": [
-//             {
-//               "pagePath": "pages/douban/in_theaters/index",
-//               "text": "豆瓣电影",
-//               "iconPath": "./image/douban.png",
-//               "selectedIconPath": "./image/douban-s.png"
-//             },
-//             {
-//               "pagePath": "pages/life/index",
-//               "text": "我的",
-//               "iconPath": "./image/life.png",
-//               "selectedIconPath": "./image/life-s.png"
-//             }
-//           ]
-// },
